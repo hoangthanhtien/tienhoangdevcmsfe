5e719a19c322004f22d62d2021dba8407402401b
Hoàng Thành Tiến <hoangthanhtien0604@gmail.com>
2022-12-15 23:47:20 +0700

    init

--- src/views/theme/colors/Colors.js
+++ src/views/theme/colors/Colors.js
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types'
-import React, { useEffect, useState, createRef } from 'react'
+import React, { useEffect, useState, createRef, useRef } from 'react'
 import classNames from 'classnames'
 import { CRow, CCol, CCard, CCardHeader, CCardBody } from '@coreui/react'
 import { rgbToHex } from '@coreui/utils'
-import { DocsLink } from 'src/components'
+import mermaid from 'mermaid'
+// import ReactMarkdown from 'react-markdown'
+// import MarkdownPreview from '@uiw/react-markdown-preview'
+// import { DocsLink } from 'src/components'
+import MDEditor from '@uiw/react-md-editor'
 
 const ThemeView = () => {
   const [color, setColor] = useState('rgb(255, 255, 255)')
@@ -47,41 +51,92 @@ ThemeColor.propTypes = {
   className: PropTypes.string,
 }
 
+const randomid = () => parseInt(String(Math.random() * 1e15), 10).toString(36)
+
+const Code = ({ inline, children = [], className, ...props }) => {
+  const demoid = useRef(`dome${randomid()}`)
+  const code = getCode(children)
+  const demo = useRef(null)
+  useEffect(() => {
+    if (demo.current) {
+      try {
+        const str = mermaid.render(demoid.current, code, () => null, demo.current)
+        // @ts-ignore
+        demo.current.innerHTML = str
+      } catch (error) {
+        // @ts-ignore
+        demo.current.innerHTML = error
+      }
+    }
+  }, [code, demo])
+
+  if (
+    typeof code === 'string' &&
+    typeof className === 'string' &&
+    /^language-mermaid/.test(className.toLocaleLowerCase())
+  ) {
+    return (
+      <code ref={demo}>
+        <code id={demoid.current} style={{ display: 'none' }} />
+      </code>
+    )
+  }
+  return <code className={String(className)}>{children}</code>
+}
+
+Code.propTypes = {
+  inline: PropTypes.any,
+  children: PropTypes.array,
+  className: PropTypes.any,
+}
+
+const getCode = (arr = []) =>
+  arr
+    .map((dt) => {
+      if (typeof dt === 'string') {
+        return dt
+      }
+      if (dt.props && dt.props.children) {
+        return getCode(dt.props.children)
+      }
+      return false
+    })
+    .filter(Boolean)
+    .join('')
+
 const Colors = () => {
+  const [markdownStr, setMarkdownStr] = useState('')
+
+  useEffect(() => {
+    import('src/assets/markdown/test-golang-doc/test-golang-doc.md').then((res) =>
+      fetch(res.default)
+        .then((response) => response.text())
+        .then((response) => setMarkdownStr(response))
+        .catch((err) => console.log(err)),
+    )
+  }, [])
+
   return (
     <>
       <CCard className="mb-4">
         <CCardHeader>
-          Theme colors
-          <DocsLink href="https://coreui.io/docs/utilities/colors/" />
+          Test coding blog
+          {/* <DocsLink href="https://coreui.io/docs/utilities/colors/" /> */}
         </CCardHeader>
         <CCardBody>
-          <CRow>
-            <ThemeColor className="bg-primary">
-              <h6>Brand Primary Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-secondary">
-              <h6>Brand Secondary Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-success">
-              <h6>Brand Success Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-danger">
-              <h6>Brand Danger Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-warning">
-              <h6>Brand Warning Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-info">
-              <h6>Brand Info Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-light">
-              <h6>Brand Light Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-dark">
-              <h6>Brand Dark Color</h6>
-            </ThemeColor>
-          </CRow>
+          {/* <MarkdownPreview source={markdownStr} /> */}
+          <div className="container">
+            <MDEditor
+              value={markdownStr}
+              onChange={setMarkdownStr}
+              previewOptions={{
+                components: {
+                  code: Code,
+                },
+              }}
+            />
+            <MDEditor.Markdown source={markdownStr} components={{ code: Code }} />
+          </div>
         </CCardBody>
       </CCard>
     </>
