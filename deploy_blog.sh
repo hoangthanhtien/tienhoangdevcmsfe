5d730c257b66bf1080ee97d0b892037f25c30a51
Tim Hoang <tim@jixie.io>
2023-10-08 19:46:04 +0700

    update build step

--- deploy_blog.sh
+++ deploy_blog.sh
@@ -1,4 +1,4 @@
 ##!/bin/bash
 npm run build &&
-rm -rf /var/www/html/tienhoangdevblog' &&
-&& scp -r build root@128.199.148.146:/var/www/html/tienhoangdevblog
+ssh root@42.96.5.18 'rm -rf /var/www/html/tienhoangdevblog' &&
+scp -r build root@42.96.5.18:/var/www/html/tienhoangdevblog
